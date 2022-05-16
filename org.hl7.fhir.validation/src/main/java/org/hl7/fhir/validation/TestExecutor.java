593b6fec14a3726d52535d0a854bded40ab6c5eb
dotasek <david.otasek@smilecdr.com>
2022-05-16 15:43:30 -0400

    running with Junit cli utils

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/TestExecutor.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/TestExecutor.java
@@ -1,7 +1,7 @@
 package org.hl7.fhir.validation;
 
 import com.google.common.reflect.ClassPath;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.collections.ArrayStack;
 import org.hl7.fhir.utilities.tests.BaseTestingUtilities;
 import org.junit.platform.console.options.CommandLineOptions;
 import org.junit.platform.console.tasks.ConsoleTestExecutor;
@@ -10,18 +10,18 @@ import org.junit.platform.launcher.listeners.TestExecutionSummary;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.instrument.Instrumentation;
-import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URLClassLoader;
+import java.nio.file.*;
+import java.security.ProtectionDomain;
 import java.util.*;
-
-import org.hl7.fhir.utilities.tests.XhtmlParserTests;
+import java.util.stream.Collectors;
 
 public class TestExecutor {
 
-  public static void printClasspath() {
+  public static void printJunitClasses() {
 
     String classpath = System.getProperty("java.class.path");
     String[] classPathValues = classpath.split(File.pathSeparator);
@@ -31,24 +31,33 @@ public class TestExecutor {
     ClassLoader classLoader =org.junit.platform.commons.util.ClassLoaderUtils.getDefaultClassLoader();
 
       ClassPath classPath = ClassPath.from(classLoader);
+
       Set<ClassPath.ClassInfo> classes = classPath.getAllClasses();
 
-      for (ClassPath.ClassInfo classInfo : classes) {
-        if (classInfo.getName().contains("junit") || classInfo.getName().contains("hl7")) {
-          System.out.println(" classInfo: " + classInfo.getName());
-        }
+      List<String> testClasses = classes.stream().filter(classInfo -> classInfo.getSimpleName().equals("Test") && classInfo.getPackageName().contains("junit"))
+        .map(classInfo -> classInfo.getResourceName())
+        .collect(Collectors.toList());
+
+      for (String aClass : testClasses.stream().sorted().collect(Collectors.toList())) {
+        String path = classLoader.getResource(aClass).toExternalForm();
+        System.out.println(path);
       }
+
     }  catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
 
+  public static void exploreDiscoveryRequestBuilder(CommandLineOptions options) {
 
   }
+
   public static void main(String[] args) {
     executeTests();
   }
   public static void executeTests() {
-    printClasspath();
+    printJunitClasses();
+
     //System.setProperty("java.locale.providers", "COMPAT");
 
     System.out.println("env : " + System.getenv("java.locale.providers"));
@@ -64,15 +73,27 @@ public class TestExecutor {
 
     BaseTestingUtilities.setFhirTestCasesDirectory("/Users/david.otasek/IN/2021-12-02-fhir-test-cases-by-directory/fhir-test-cases");
 
+
     clo.setScanClasspath(true);
 
+    Path myNewPath = null;
+
+    myNewPath = Paths.get("/Users/david.otasek/IdeaProjects/org.hl7.fhir.core/org.hl7.fhir.validation/target/org.hl7.fhir.validation-5.6.46-SNAPSHOT-tests.jar");
+
+    if(!Files.exists(myNewPath, new LinkOption[0])) {
+      throw new RuntimeException("BLAH");
+    }
+
+    clo.setAdditionalClasspathEntries(Arrays.asList(myNewPath));
     //clo.setSelectedPackages(Arrays.asList(XhtmlParserTests.class.getPackage().getName()));
 
-    //clo.setIncludedEngines(Arrays.asList("junit-vintage","junit-jupiter"));
+    clo.setIncludedEngines(Arrays.asList("junit-vintage","junit-jupiter"));
     //clo.setExcludedPackages(Arrays.asList("org.hl7.fhir.r4.test"));
     //clo.setIncludedPackages(Arrays.asList("org.hl7.fhir.validation.tests"));
     try {
       ConsoleTestExecutor cte = new ConsoleTestExecutor(clo);
+
+      exploreDiscoveryRequestBuilder(clo);
 
       TestExecutionSummary testExecutionSummary = cte.execute(out);
 
@@ -84,4 +105,15 @@ public class TestExecutor {
       System.err.println();
     }
   }
+
+  private static void printClassPath() {
+    ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+    URL[] urls = ((URLClassLoader)cl).getURLs();
+
+    for(URL url: urls){
+      System.out.println(url.getFile());
+    }
+  }
+
 }
